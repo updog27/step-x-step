bd193868bcb2981f32314351a2cafbae20ee471a
updog27 <lrleyva1@hotmail.com>
2026-03-05 16:41:35 -0500

    working on errors and added api

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ async function generateRoutes() {
       const response = await fetch("https://api.openrouteservice.org/v2/directions/foot-walking", {
         method: "POST",
         headers: {
-          "Authorization": "YOUR_ORS_API_KEY", // <-- Replace with your walking-enabled key
+          "Authorization": "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjYzZjhiZWRhMmFhMzQyM2E5ODk1ZGZiM2I4ZWExNmIyIiwiaCI6Im11cm11cjY0In0=", // <-- Replace with your walking-enabled key
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
@@ -145,3 +145,4 @@ async function generateRoutes() {
 // Button Event
 // ----------------------
 startButton.addEventListener("click", generateRoutes);
+
